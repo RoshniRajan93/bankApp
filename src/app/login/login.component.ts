e3c7f11e03cb96002d8375103abd4719539a68a2
Roshni <roshni.nirappel@gmail.com>
2022-04-01 13:32:15 +0530

    Data Binding

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+aim="Your Perfect Banking Partner"
+accNum="Account Number Please!!!"
+acno=""
+pswd=""
+  // Database
+
+  database={
+    1000:{acno:1000,uname:"Neer",password:1000,balance:5000},
+    1001:{acno:1001,uname:"Laisha",password:1001,balance:3000},
+    1002:{acno:1002,uname:"Vyom",password:1002,balance:4000}
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  acnoChange(event:any){
+    //console.log(event);
+    this.acno=event.target.value
+    console.log(this.acno);
+    
+  }
+
+  pswdChange(event:any){
+    this.pswd=event.target.value
+    console.log(this.pswd);
+    
+  }
+
+  login(){
+    alert("Login Clicked !!!")
+  }
+
+  
 }
